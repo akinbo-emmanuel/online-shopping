fa4d0a25dc91fc01cd960c6e5d66a87d47e7eda6
akinbo-emmanuel <akinbo.emmanuel@lmu.edu.ng>
2025-03-26 17:02:21 +0100

    Minor responsiveness fix

--- components/home/Features.tsx
+++ components/home/Features.tsx
@@ -30,7 +30,7 @@ const Features = () => {
     <section className="shadow-[0px_20px_52.29px_0px_#4444440A] py-[72px] bg-white">
       <div className="grid grid-cols-2 lg:grid-cols-3 xl:flex xl:flex-wrap items-center justify-between gap-x-2.5 md:gap-x-5 gap-y-5 md:gap-y-10 flex-wrap w-screen max-w-[95vw] 2xl:max-w-[1280px] mx-auto">
         {features.map((feature, index) => (
-          <div key={index} className="flex items-center gap-3.5">
+          <div key={index} className="flex items-center gap-2 md:gap-3.5">
             <Image
               src={feature.icon}
               alt={feature.title}
