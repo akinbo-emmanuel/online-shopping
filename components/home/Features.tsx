4dd55f67a306d171608451fc567fabdc6ce3ca3b
akinbo-emmanuel <akinbo.emmanuel@lmu.edu.ng>
2025-03-26 17:01:52 +0100

    Minor fix

--- components/home/Features.tsx
+++ components/home/Features.tsx
@@ -28,7 +28,7 @@ const features = [
 const Features = () => {
   return (
     <section className="shadow-[0px_20px_52.29px_0px_#4444440A] py-[72px] bg-white">
-      <div className="grid grid-cols-2 lg:grid-cols-3 xl:flex xl:flex-wrap items-center justify-between gap-x-5 gap-y-10 flex-wrap w-screen max-w-[95vw] 2xl:max-w-[1280px] mx-auto">
+      <div className="grid grid-cols-2 lg:grid-cols-3 xl:flex xl:flex-wrap items-center justify-between gap-x-2.5 md:gap-x-5 gap-y-5 md:gap-y-10 flex-wrap w-screen max-w-[95vw] 2xl:max-w-[1280px] mx-auto">
         {features.map((feature, index) => (
           <div key={index} className="flex items-center gap-3.5">
             <Image
@@ -38,7 +38,7 @@ const Features = () => {
               height={50}
               className="w-10 md:w-auto h-10 md:h-auto object-contain"
             />
-            <div className="text-[#484848] font-poppins">
+            <div className="text-[#484848] font-poppins space-y-1">
               <h4 className="font-medium text-base/[100%] md:text-xl/[100%] capitalize">
                 {feature.title}
               </h4>
